af153db96bb3f65d70303d9b64c204b11ff0ad7c
Yamina-S <61542053+Yamina-S@users.noreply.github.com>
2023-09-10 19:14:05 +0200

    update HUB and fix track

--- src/scenes/Track/HUD.gd
+++ src/scenes/Track/HUD.gd
@@ -1,30 +1,56 @@
 extends CanvasLayer
 
+# Countdown
+export (int) var minutes = 0
+export (int) var seconds = 0
+
+var dsec = 0
+
 signal start_game
 
 func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+	$Control/Message.text = text
+	$Control/Message.show()
 
 func show_game_over():
 	show_message("Game Over")
 	# Wait until the MessageTimer has counted down.
-	yield($MessageTimer, "timeout")
+	yield($Control/Timer, "timeout")
 
-	$Message.text = "Ready?"
-	$Message.show()
+	$Control/Message.text = "Ready?"
+	$Control/Message.show()
 	# Make a one-shot timer and wait for it to finish.
-	yield(get_tree().create_timer(1), "timeout")
-	$StartButton.show()
+	yield(get_tree().create_timer(5), "timeout")
+	$Control/StartButton.show()
 
-func update_score(score):
-	$ScoreLabel.text = str(score)
-
-func _on_MessageTimer_timeout():
-	$Message.hide()
+func _on_GameTimer_timeout():
+	get_tree().change_scene("res://src/scenes/Menu/menu.tscn")
 
 
 func _on_StartButton_pressed():
-	$StartButton.hide()
+	$Control/StartButton.hide()
+	$Control/Message.hide()
+	$Control/GameTimer.start()
+	dsec -= 1
 	emit_signal("start_game")
+
+func _physics_process(delta):
+	if seconds > 0 and dsec <= 0:
+		seconds -=1
+		dsec = 10
+	if minutes > 0 and seconds <=0:
+		minutes -= 1
+		seconds = 60
+		
+	if minutes >= 10:
+		$Control/sec.set_text(str(minutes))
+	else:
+		$Control/sec.set_text("0"+str(minutes))	
+	if seconds >= 10:
+		$Control/sec.set_text(str(seconds))
+	else:
+		$Control/sec.set_text("0"+str(seconds))	
+	if dsec >= 10:
+		$Control/sec.set_text(str(dsec))
+	else:
+		$Control/sec.set_text("0"+str(dsec))
