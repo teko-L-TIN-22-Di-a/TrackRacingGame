0be0ab873827c6d66cf64865f9cfe5593cec665f
Yamina-S <61542053+Yamina-S@users.noreply.github.com>
2023-09-10 14:31:16 +0200

    update Track
    update Track
    update folder structure

--- src/scenes/Track/track.gd
+++ src/scenes/Track/track.gd
@@ -1,19 +1,10 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var score
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func game_over():
 	$ScoreTimer.stop()
 
@@ -24,7 +15,6 @@ func new_game():
 
 func _on_ScoreTimer_timeout():
 	score += 1
-
 
 func _on_StartTimer_timeout():
 	$ScoreTimer.start()
