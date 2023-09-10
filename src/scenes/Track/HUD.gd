b07162f84477a25fe5bd1e8e2816e9332582018a
Yamina-S <61542053+Yamina-S@users.noreply.github.com>
2023-09-10 22:44:15 +0200

    wip Countdown fix
    Does not work as expected.
    Counts to fast

--- src/scenes/Track/HUD.gd
+++ src/scenes/Track/HUD.gd
@@ -5,6 +5,7 @@ export (int) var minutes = 0
 export (int) var seconds = 0
 
 var dsec = 0
+var delta = 5
 
 signal start_game
 
@@ -30,22 +31,18 @@ func _on_GameTimer_timeout():
 func _on_StartButton_pressed():
 	$Control/StartButton.hide()
 	$Control/Message.hide()
-	$Control/GameTimer.start()
-	dsec -= 1
 	emit_signal("start_game")
 
 func _physics_process(delta):
-	if seconds > 0 and dsec <= 0:
+	if dsec >= 0:
+		dsec -=1
+	if seconds > 0 and dsec <= 0 :
 		seconds -=1
 		dsec = 10
 	if minutes > 0 and seconds <=0:
 		minutes -= 1
 		seconds = 60
 		
-	if minutes >= 10:
-		$Control/sec.set_text(str(minutes))
-	else:
-		$Control/sec.set_text("0"+str(minutes))	
 	if seconds >= 10:
 		$Control/sec.set_text(str(seconds))
 	else:
--- src/scenes/Track/track.gd
+++ src/scenes/Track/track.gd
@@ -9,12 +9,8 @@ func game_over():
 	$ScoreTimer.stop()
 
 func new_game():
-	score = 0
 	#$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-func _on_ScoreTimer_timeout():
-	score += 1
 
 func _on_StartTimer_timeout():
 	$ScoreTimer.start()
