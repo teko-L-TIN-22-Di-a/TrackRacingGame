3fb5e765cfa3aee4c663bfc2a2cda981171b4813
TheCodeWizard27 <benny.schaerer@gmail.com>
2023-09-11 15:18:35 +0200

    Added release fixes and minor tweaks.

--- src/scenes/Track/HUD.gd
+++ src/scenes/Track/HUD.gd
@@ -1,53 +1,31 @@
+class_name Hud
 extends CanvasLayer
 
-# Countdown
-export (int) var minutes = 0
-export (int) var seconds = 0
+onready var timer_label: Label = $'Control/CenterContainer/TimerLabel'
+onready var message_label: Label = $'Control/CenterContainer2/MessageLabel'
+onready var score_board: VBoxContainer = $'Control/ScoreBoard'
 
-var dsec = 0
-var delta = 5
+func show_message(text: String) -> void:
+	message_label.text = text
+	message_label.show()
+	
+func hide_message() -> void:
+	message_label.hide()
 
-signal start_game
+func update_timer(unix_time: int) -> void:
+	timer_label.show()
+	timer_label.text = _unix_time_to_string(unix_time)
 
-func show_message(text):
-	$Control/Message.text = text
-	$Control/Message.show()
+func hide_timer() -> void:
+	timer_label.hide()
 
-func show_game_over():
-	show_message("Game Over")
-	# Wait until the MessageTimer has counted down.
-	yield($Control/Timer, "timeout")
+func add_time(unix_time: int) -> void:
+	var new_label = Label.new()
+	new_label.text = _unix_time_to_string(unix_time)
+	score_board.add_child(new_label)
+	score_board.move_child(new_label, 0)
 
-	$Control/Message.text = "Ready?"
-	$Control/Message.show()
-	# Make a one-shot timer and wait for it to finish.
-	yield(get_tree().create_timer(5), "timeout")
-	$Control/StartButton.show()
-
-func _on_GameTimer_timeout():
-	get_tree().change_scene("res://src/scenes/Menu/menu.tscn")
-
-
-func _on_StartButton_pressed():
-	$Control/StartButton.hide()
-	$Control/Message.hide()
-	emit_signal("start_game")
-
-func _physics_process(delta):
-	if dsec >= 0:
-		dsec -=1
-	if seconds > 0 and dsec <= 0 :
-		seconds -=1
-		dsec = 10
-	if minutes > 0 and seconds <=0:
-		minutes -= 1
-		seconds = 60
-		
-	if seconds >= 10:
-		$Control/sec.set_text(str(seconds))
-	else:
-		$Control/sec.set_text("0"+str(seconds))	
-	if dsec >= 10:
-		$Control/sec.set_text(str(dsec))
-	else:
-		$Control/sec.set_text("0"+str(dsec))
+func _unix_time_to_string(unix_time: int) -> String:
+	var minutes = unix_time / 60
+	var seconds = unix_time % 60
+	return "%02d : %02d" % [minutes, seconds]
--- src/scenes/Track/track.gd
+++ src/scenes/Track/track.gd
@@ -1,16 +1,71 @@
 extends Spatial
 
-var score
+onready var player: Player = $'Player'
+onready var start_pos: Position3D = $'StartPosition'
+
+onready var hud: Hud = $'HUD'
+
+onready var start_timer: Timer = $'StartTimer'
+
+var countdown: int
+var started = false
+var start_time: int
+var midpoint_reached = false
+
+func _process(delta):
+	
+	if(!started): 
+		player.velocity = Vector3.ZERO
+		return
+	
+	hud.update_timer(OS.get_unix_time() - start_time)
 
 func _ready():
 	new_game()
 
 func game_over():
-	$ScoreTimer.stop()
+	pass
 
 func new_game():
-	#$Player.start($StartPosition.position)
-	$StartTimer.start()
+	player.transform.origin = start_pos.transform.origin
+	player.disable()
+	
+	countdown = 3
+	hud.show_message("Ready? \n 3")
+	hud.hide_timer()
+	start_timer.start()
+	
+func start_game():
+	pass
 
 func _on_StartTimer_timeout():
-	$ScoreTimer.start()
+	
+	if(countdown < 0):
+		start_game()
+		hud.hide_message()
+		start_timer.stop()
+		player.enable()
+		start_time = OS.get_unix_time()
+		started = true
+		
+		return
+	
+	hud.show_message("Ready? \n %d" % [countdown])
+	countdown -= 1
+
+
+func _on_Finish_body_entered(body: Node):
+	if(body.name != "KinematicBody"): # Player
+		return
+		
+	if(!midpoint_reached): return
+	
+	hud.add_time(OS.get_unix_time() - start_time)
+	start_time = OS.get_unix_time()
+	midpoint_reached = false
+
+func _on_Midpoint_body_entered(body: Node):
+	if(body.name != "KinematicBody"): # Player
+		return
+		
+	midpoint_reached = true
